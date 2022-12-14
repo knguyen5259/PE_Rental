0ccdcca1f491a4449f90c9f114bd2f4ea6746b8d
JoelJohn <jjohnn3593@stu.d214.org>
2022-12-14 11:12:36 -0600

    made ui button

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            Button {
+                print("Button pressed")
+            } label: {
+                Text("Uibutton ")
+            }
+
         }
     }
 }
