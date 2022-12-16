d163caa4247a795ead95d90c979b580d13c22400
knguyen5259 <knguyen5259@stu.d214.org>
2022-12-16 10:44:49 -0600

    Added database file

---
+++ Database.swift
@@ -0,0 +1,14 @@
+//
+//  Database.swift
+//  PE_Rental
+//
+//  Created by Keenan Nguyen on 12/16/22.
+//
+
+import Foundation
+import Firebase
+
+
+class Database {
+//    let database = Firebase
+}
